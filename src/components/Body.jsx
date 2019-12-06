1e6477576cc597167985dac7b8ddacd5ec6866f2
Andrew Hollingworth <andrew.hollingworth@gmail.com>
2019-12-06 14:12:05 -0500

    Add additional prompts. Slight formatting changes.

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -6,8 +6,8 @@ const useStyles = makeStyles(theme => ({
   intro: {
     fontFamily: 'Poppins, sans-serif',
     fontSize: '1em',
-    marginTop: '5em',
     textAlign: 'center',
+    marginTop: '5em',
   },
   prompt: {
     fontFamily: 'Poppins, sans-serif',
@@ -30,9 +30,14 @@ export default function Body() {
     'What\'s the weirdest bug you\'ve found?',
     'How did you start working with this company?',
     'What do you look for in a candidate?',
+    'What are you working on in your free time?',
+    'What\'s the most interesting thing you\'ve learned lately?',
+    'What are you geeking out about right now?',
+    'Where was the last place you traveled?',
+    'What is the best thing that happened to you today?',
   ];
 
-  const [currentPrompt, setPrompt] = useState(0);
+  const [currentPrompt, setPrompt] = useState(Math.floor(Math.random() * promptSeed.length));
 
   const changePrompt = () => {
     const newIndex = Math.floor(Math.random() * promptSeed.length);
